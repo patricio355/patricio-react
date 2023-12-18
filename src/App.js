54c1d607700825122e0ed184299113ead92f6bb9
patricio355 <PatricioQuiroga35@gmail.com>
2023-12-17 23:56:08 -0300

    nuevo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          buenas noches
+          buenas noche
         </p>
         <a
           className="App-link"
